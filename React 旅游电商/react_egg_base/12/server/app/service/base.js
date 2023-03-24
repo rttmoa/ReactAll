e8f0291cd4891805b7c797665dd44290a6ae5f04
rttmoa <rttmoa@gmail.com>
2023-03-24 17:33:37 +0800

    Update

---
+++ React 旅游电商/react_egg_base/12/server/app/service/base.js
@@ -0,0 +1,16 @@
+const Service = require('egg').Service;
+
+class BseService extends Service {
+  run(callback) {
+    const { ctx, app } = this;
+    try {
+      if(callback)
+        return callback(ctx, app);
+    } catch (error) {
+      console.log('报错了',error);
+      return null;
+    }
+  }
+}
+
+module.exports = BseService;
