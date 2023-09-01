0f2f10d50dd1cc18c59d7b454263c8537c2a5f4d
rttmoa <rttmoa@gmail.com>
2023-09-01 13:16:09 +0800

    -

--- react/react-redux-dianping/mock/server.js
+++ react/react-redux-dianping/mock/server.js
@@ -1,25 +1,27 @@
-var app = require('koa')();
-var router = require('koa-router')();
+var app = require('koa')()
+var router = require('koa-router')()
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
 // router.get('/', function *(next) {
 //     this.body = 'hello koa !'
 // });
 
 // router.get('/api', function *(next) {
+//     console.log(123)
 //     this.body = 'test data'
 // });
 
-// 首页 —— 广告（超值特惠）
+// 首页 —— 广告（超值特惠）   // http://localhost:3000/api/homead
 var homeAdData = require('./home/ad.js')
-router.get('/api/homead', function (next) {
+router.get('/api/homead', function (next) { 
     console.log('首页 —— 广告（超值特惠）')
-
+    // console.log(this.response)
     this.body = homeAdData
-});
+})
 
-// 首页 —— 推荐列表（猜你喜欢）
+// 首页 —— 推荐列表（猜你喜欢） // http://localhost:3000/api/homelist/:city/:page
 var homeListData = require('./home/list.js')
-router.get('/api/homelist/:city/:page', function (next) {
+router.get('/api/homelist/:city/:page', function (next) { 
     console.log('首页 —— 推荐列表（猜你喜欢）')
 
     // 参数
@@ -31,11 +33,13 @@ router.get('/api/homelist/:city/:page', function (next) {
     console.log('当前页数：' + paramsPage)
 
     this.body = homeListData
-});
+})
 
-// 搜索结果页 - 搜索结果 - 三个参数
+// 搜索结果页 - 搜索结果 - 三个参数  
+// http://localhost:3000/api/search/222/beijing/scenery/Forbidden
 var searchListData = require('./search/list.js')
-router.get('/api/search/:page/:city/:category/:keyword', function(next) {
+// 参数必传：页码、城市、分类、关键词
+router.get('/api/search/:page/:city/:category/:keyword', function (next) {
     console.log('搜索结果页 - 搜索结果')
 
     // 参数
@@ -53,8 +57,8 @@ router.get('/api/search/:page/:city/:category/:keyword', function(next) {
     this.body = searchListData
 })
 
-// 搜索结果页 - 搜索结果 - 两个参数
-router.get('/api/search/:page/:city/:category', function(next) {  //->   /api/search/0/北京/all/UserName长
+// 搜索结果页 - 搜索结果 - 两个参数   // http://localhost:3000/api/search/222/beijing/scenery
+router.get('/api/search/:page/:city/:category', function (next) {  //->   
     console.log('搜索结果页 - 搜索结果')
 
     // 参数
@@ -70,9 +74,9 @@ router.get('/api/search/:page/:city/:category', function(next) {  //->   /api/se
     this.body = searchListData
 })
 
-// 详情页 - 商户信息
+// 详情页 - 商户信息    // http://localhost:3000/api/detail/info/5934707649654805
 const detailInfo = require('./detail/info.js')
-router.get('/api/detail/info/:id', function(next) {   //-->    /api/detail/info/5934707649654805
+router.get('/api/detail/info/:id', function (next) {   //-->    
     console.log('详情页 - 商户信息')
 
     const params = this.params
@@ -83,14 +87,14 @@ router.get('/api/detail/info/:id', function(next) {   //-->    /api/detail/info/
     this.body = detailInfo
 })
 
-// 详情页 - 用户评论
+// 详情页 - 用户评论  /// http://localhost:3000/api/detail/comment/5/5934707649654805
 const detailComment = require('./detail/comment.js')
-router.get('/api/detail/comment/:page/:id', function(next) {
+router.get('/api/detail/comment/:page/:id', function (next) {
     console.log('详情页 - 用户点评')
 
     const params = this.params
     const page = params.page
-    const id = params.id  
+    const id = params.id
 
     console.log('商户id: ' + id)
     console.log('当前页数: ' + page)
@@ -98,22 +102,24 @@ router.get('/api/detail/comment/:page/:id', function(next) {
     this.body = detailComment
 })
 
-// 订单列表
+// 订单列表  /// http://localhost:3000/api/orderlist/zhangsan
 const orderList = require('./orderlist/orderList.js')
-router.get('/api/orderlist/:username', function(next) { //->   /api/orderlist/zhangsan
+router.get('/api/orderlist/:username', function (next) {  
     console.log('订单列表')
 
     const params = this.params
     const username = params.username
     console.log('用户名：' + username)
-    
-    console.log('orderList', orderList)
+
+    // console.log('orderList', orderList)
     this.body = orderList
 })
-
-// 提交评论
-router.post('/api/submitComment', function(next) { 
+ 
+// 提交评论  //// http://localhost:3000/api/submitComment
+router.post('/api/submitComment', function (next) {
+    
     console.log('提交评论', this)
+    // console.log(this)
 
     // 获取参数 
     this.body = {
@@ -122,6 +128,37 @@ router.post('/api/submitComment', function(next) {
     }
 })
 
-// 开始服务并生成路由
-app.use(router.routes()).use(router.allowedMethods());
-app.listen(3000);
+
+
+// FIXME: 订单列表  /// http://localhost:3000/api/orderlistTest
+let url1 = "/api/orderlistTest/:username/";   // http://localhost:3000/api/orderlistTest/zhangsan
+let url2 = "/api/orderlistTest/:username/:city";  // http://localhost:3000/api/orderlistTest/zhangsan/beijing
+let url3 = "/api/orderlistTest/";
+router.get(url3, function (next) {  
+    console.log('订单列表')
+
+
+
+    console.log(this.url) //   /api/orderlistTest?username=zhangsan&age=123
+    console.log(this.search) //   ?username=zhangsan&age=123
+    console.log(this.query) //   [Object: null prototype] { username: 'zhangsan', age: '123' }
+    console.log(this.pathname) // undefined
+    console.log(this.path) // /api/orderlistTest
+    console.log(this.href) // http://localhost:3000/api/orderlistTest?username=zhangsan&age=123
+ 
+    // const params = this.params
+    // const username = params.username
+    // console.log('用户名：' + username)
+
+    this.body = "订单列表返回Success！"
+})
+
+
+
+// TODO: 开始服务并生成路由
+app.use(
+    router.routes()
+).use(
+    router.allowedMethods()
+)
+app.listen(3000)
