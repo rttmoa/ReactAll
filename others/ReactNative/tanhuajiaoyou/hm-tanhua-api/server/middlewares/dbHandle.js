25119f35b6256ff6f377be66b61804f5e7e4337f
rttmoa <rttmoa@gmail.com>
2023-09-01 13:45:54 +0800

    -

--- others/ReactNative/tanhuajiaoyou/hm-tanhua-api/server/middlewares/dbHandle.js
+++ others/ReactNative/tanhuajiaoyou/hm-tanhua-api/server/middlewares/dbHandle.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql')
 // 创建mysql线程池
 let pool = mysql.createPool({
     user: 'root',
@@ -6,8 +6,8 @@ let pool = mysql.createPool({
     database: 'tanhua',
     host: '127.0.0.1',
     port: '3306'
-});
- 
+})
+
 const dbHandle = () => {
     // 创建方法 ,sql:sql语句，values：字段对应的值
     // 例如： insert into users(name) values (?) 中的 ? 就是从values中取得
@@ -15,12 +15,12 @@ const dbHandle = () => {
         return async (sql, values) => {
             // console.log(sql)
             // 比如手机号获取验证码接口：
-                // select status from dt_users where mobile =15303663375 
-                // update dt_users set vcode = 888888,login_time=NOW() where mobile =15303663375
+            // select status from dt_users where mobile =15303663375 
+            // update dt_users set vcode = 888888,login_time=NOW() where mobile =15303663375
             // console.log(values)
             return new Promise((resolve, reject) => {
                 pool.getConnection(function (err, connection) {
-                    if (err) { reject(err) } 
+                    if (err) { reject(err) }
                     else {
                         connection.query(sql, values, (err, fields) => {
                             // console.log(fields)
@@ -36,7 +36,7 @@ const dbHandle = () => {
                             //   }
                             if (err) reject(err)
                             else resolve(fields)
-                            connection.release();
+                            connection.release()
                         })
                     }
                 })
@@ -45,10 +45,10 @@ const dbHandle = () => {
     }
 
     return async (ctx, next) => {
-        ctx.executeSql = execute();  // 注册全局ctx
+        ctx.executeSql = execute()  // 注册全局ctx
         // console.log(ctx.executeSql) // [AsyncFunction (anonymous)]
-        await next();
+        await next()
     }
 }
 
-module.exports = dbHandle;
+module.exports = dbHandle
